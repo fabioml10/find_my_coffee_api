c01771c587d65a8eb22d2a54cd680fe41e14fbc0
Fabio Muller <fabioml10@yahoo.com.br>
2020-10-20 19:45:42 -0300

    Stores endpoint done

--- app/controllers/api/v1/stores_controller.rb
+++ app/controllers/api/v1/stores_controller.rb
@@ -3,10 +3,17 @@ class Api::V1::StoresController < ApplicationController
 
   def show; end
 
+  def index
+    @stores = Store.within(params[:longitude].to_f, params[:latitude].to_f, params[:distance_in_km].to_i)
+      .sort_by { |store| store.ratings_average }
+      .reverse
+  end
+
   private
 
     def set_store
       # ! dá erro 404 se não encontrado
       @store = Store.find_by!(google_place_id: params[:id])
     end
+
 end
